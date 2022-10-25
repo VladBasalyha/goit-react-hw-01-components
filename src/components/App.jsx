102537bf6d6b1e100ed7cd9424bae7c1310ed2b1
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2022-10-25 09:04:10 +0300

    some upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
+      <p>хуйаьвіщфавіф</p>
       React homework template
     </div>
   );
