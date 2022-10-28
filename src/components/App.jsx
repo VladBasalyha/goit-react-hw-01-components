49e173c490eb6cde23ee86401fe40780a3e42f70
Vladyslav Basalyha <100045506+VladBasalyha@users.noreply.github.com>
2022-10-28 23:37:58 +0300

    some stuff

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,16 @@
+import { UserProfile } from './User';
+import user from '../user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <p>хуйаьвіщфавіф</p>
-      React homework template
+    <div>
+      <UserProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
